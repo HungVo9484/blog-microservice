9419afc54a52d003cf2f80bb4f417b2bc8c076ab
HungVo9484 <hung.vo9484@gmail.com>
2021-09-06 08:46:35 +0700

    Add Moderation Service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,9 +3,22 @@ import styled from 'styled-components';
 const Styles = styled.div``;
 
 const CommentList = ({ comments }) => {
-  const renderedList = comments.map((comment) => (
-    <li key={comment.id}>{comment.content}</li>
-  ));
+  const renderedList = comments.map((comment) => {
+    let content;
+
+    if (comment.status === 'approved') {
+      content = comment.content;
+    }
+
+    if (comment.status === 'pending') {
+      content = 'this comment is awaiting moderation.';
+    }
+
+    if (comment.status === 'rejected') {
+      content = 'this comment has been rejected.';
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
   return (
     <Styles>
       <ul>{renderedList}</ul>
